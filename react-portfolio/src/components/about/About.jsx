b77a98104c78a90ad4034a6cfca11dff02dee0bd
unknown <corentin.malbrun1@gmail.com>
2022-07-16 09:37:05 +0200

    nav+about

--- react-portfolio/src/components/about/About.jsx
+++ react-portfolio/src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {BsAward} from 'react-icons/bs'
+import {FiUsers} from 'react-icons/fi'
+import {AiOutlineFolderAdd} from 'react-icons/ai'
 
-export const About = () => {
+const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5> Me connaitre </h5>
+      <h2> A propos de moi</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <BsAward className='about__icon'/>
+              <h5> Experience</h5>
+              <small> +1 an d'experience</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5> Clients</h5>
+              <small> 200+ world</small>
+            </article>
+
+            <article className='about__card'>
+              <AiOutlineFolderAdd className='about__icon'/>
+              <h5> Projet</h5>
+              <small> +3 projets complétés </small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet nobis doloribus corporis aut molestias aliquid nam dicta neque rerum facere, illo ipsum quibusdam autem voluptatem nemo dolorum minus officiis quia?
+          </p>
+
+          <a href="#contact" className='btn btn-primary'> Me contacter </a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- react-portfolio/src/components/nav/Nav.jsx
+++ react-portfolio/src/components/nav/Nav.jsx
@@ -5,15 +5,17 @@ import {BiUser} from 'react-icons/bi'
 import {BiBook} from 'react-icons/bi'
 import {RiServiceLine} from 'react-icons/ri'
 import {AiOutlineMessage} from 'react-icons/ai'
+import {useState} from 'react'
 
-function Nav() {
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#")
   return (
     <nav>
-      <a href="#"><AiOutlineHome /></a>
-      <a href="#about"><BiUser /></a>
-      <a href="#experience"><BiBook /></a>
-      <a href="#job"><RiServiceLine /></a>
-      <a href="#contact"><AiOutlineMessage  /></a>
+      <a href="#" onClick={() => setActiveNav("#")} className={activeNav === "#" ? "active" : ""}> <AiOutlineHome /></a>
+      <a href="#about" onClick={() => setActiveNav("#about")} className={activeNav === "#about" ? "active" : ""}><BiUser /></a>
+      <a href="#experience" onClick={() => setActiveNav("#experience")} className={activeNav === "#experience" ? "active" : ""}><BiBook /></a>
+      <a href="#job" onClick={() => setActiveNav("#job")} className={activeNav === "#job" ? "active" : ""}><RiServiceLine /></a>
+      <a href="#contact" onClick={() => setActiveNav("#contact")} className={activeNav === "#contact" ? "active" : ""}><AiOutlineMessage  /></a>
     </nav>
   )
 }
